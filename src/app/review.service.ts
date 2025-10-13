c52aa1584e5f2ad46eb1de27a70475483d9c2cfa
Rajasekhar Cherukuri <craja79@gmail.com>
2025-10-13 11:53:28 -0400

    connect to online api
    connect to online api

--- src/app/review.service.ts
+++ src/app/review.service.ts
@@ -3,15 +3,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, finalize } from 'rxjs';
 import { ReviewRequest, ReviewResponse } from './models';
+import { environment } from '../environments/environment';
+
 
 @Injectable({ providedIn: 'root' })
 export class ReviewService {
   loading = false;
   constructor(private http: HttpClient) {}
 
-  query(req: ReviewRequest): Observable<ReviewResponse> {
+  query(body: ReviewRequest): Observable<ReviewResponse> {
     this.loading = true;
-    return this.http.post<ReviewResponse>('/api/reviews/query', req)
-      .pipe(finalize(() => this.loading = false));
+    return new Observable<ReviewResponse>(observer => {
+      this.http.post<ReviewResponse>(`${environment.apiBase}/api/reviews/query`, body)
+        .subscribe({
+          next: (res) => { this.loading = false; observer.next(res); observer.complete(); },
+          error: (err) => { this.loading = false; observer.error(err); }
+        });
+    });
   }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiBase: 'https://movie-reviews-api.onrender.com'
+};
